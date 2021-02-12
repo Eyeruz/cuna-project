ca9033e9d83ae59e7ad679185c4954dcbf42d75c
Eyeruz <3yerus@gmail.com>
2021-02-11 20:06:41 -0500

    added Formik yup and email-validator

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Formik } from "formik";
+import * as EmailValidator from "email-validator";
+import * as Yup from "yup";
 
 // [x] Auto Purchase Price (Currency)
 // [x] Auto Make (Text)
@@ -12,85 +15,147 @@ import React, { useState } from "react";
 // and controls for moving forward.
 
 const Home = () => {
-  const [autoPrice, setAutoPrice] = useState(0);
-  const [autoMake, setAutoMake] = useState("");
-  const [autoModel, setAutoModel] = useState("");
-  const [yearlyIncome, setYearlyIncome] = useState(0);
-  const [creditScore, setCreditScore] = useState(300);
+  //   const [autoPrice, setAutoPrice] = useState(0);
+  //   const [autoMake, setAutoMake] = useState("");
+  //   const [autoModel, setAutoModel] = useState("");
+  //   const [yearlyIncome, setYearlyIncome] = useState(0);
+  //   const [creditScore, setCreditScore] = useState(300);
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     setAutoPrice(0);
+  //     setAutoMake("");
+  //     setAutoModel("");
+  //     setYearlyIncome(0);
+  //     setCreditScore(350);
+  //   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setAutoPrice(0);
-    setAutoMake("");
-    setAutoModel("");
-    setYearlyIncome(0);
-    setCreditScore(350);
-  };
   return (
-    <div>
-      <h1>Auto Loan</h1>
-      <h3> Fill out this form to pre qualify for an auto loan </h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="autoPrice"> Auto Purchase Price: </label>
-        <input
-          type="number"
-          min="1"
-          id="autoPrice"
-          name="autoPrice"
-          value={autoPrice}
-          placeholder="Auto Purchase Price"
-          onChange={(e) => setAutoPrice(e.target.value)}
-        />
-        <label htmlFor="autoMake"> Auto Make: </label>
+    <Formik
+      initialValues={{
+        autoPrice: "",
+        autoMake: "",
+        autoModel: "",
+        yearlyIncome: "",
+        creditScore: "",
+      }}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        setTimeout(() => {
+          console.log("Loading...", values);
+          setSubmitting(false);
+          resetForm();
+        }, 500);
+      }}
+      validationSchema={Yup.object().shape({
+        autoPrice: Yup.string().required("No Price Provided"),
+        autoMake: Yup.string().required("No Make Provided"),
+        autoModel: Yup.string().required("No Model provided"),
+        yearlyIncome: Yup.string().required("No Yearly Income Provided"),
+        creditScore: Yup.string()
+          .required("No Credit Score provided.")
+          .matches(
+            /(?=.*[350-850])/,
+            "credit score must be with in a 350 - 850 range"
+          ),
+      })}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleSubmit,
+        } = props;
+        return (
+          <form onSubmit={handleSubmit}>
+            <h1>Auto Loan</h1>
 
-        <input
-          type="text"
-          id="autoMake"
-          name="autoMake"
-          value={autoMake}
-          placeholder="Auto Make"
-          onChange={(e) => setAutoMake(e.target.value)}
-        />
-        <label htmlFor="autoModel"> Auto Model: </label>
+            <h3> Fill out this form to pre qualify for an auto loan </h3>
+            <label htmlFor="autoPrice"> Auto Purchase Price: </label>
+            <input
+              type="number"
+              min="1"
+              id="autoPrice"
+              name="autoPrice"
+              value={values.autoPrice}
+              placeholder="Auto Purchase Price"
+              onChange={handleChange}
+              className={errors.autoPrice && touched.autoPrice && "error"}
+            />
+            {errors.autoPrice && touched.autoPrice && (
+              <div className="input-error">{errors.autoPrice}</div>
+            )}
 
-        <input
-          type="text"
-          id="autoModel"
-          name="autoModel"
-          value={autoModel}
-          placeholder="Auto Model"
-          onChange={(e) => setAutoModel(e.target.value)}
-        />
+            <label htmlFor="autoMake"> Auto Make: </label>
 
-        <label htmlFor="yearlyIncome"> Yearly Income: </label>
+            <input
+              type="text"
+              id="autoMake"
+              name="autoMake"
+              value={values.autoMake}
+              placeholder="Auto Make"
+              onChange={handleChange}
+              className={errors.autoMake && touched.autoMake && "error"}
+            />
+            {errors.autoMake && touched.autoMake && (
+              <div className="input-error">{errors.autoMake}</div>
+            )}
+            <label htmlFor="autoModel"> Auto Model: </label>
 
-        <input
-          type="integer"
-          id="yearlyIncome"
-          name="yearlyIncome"
-          value={yearlyIncome}
-          placeholder="Yearly Income"
-          onChange={(e) => setYearlyIncome(e.target.value)}
-        />
-        <label htmlFor="creditScore"> Credit Score: </label>
+            <input
+              type="text"
+              id="autoModel"
+              name="autoModel"
+              value={values.autoModel}
+              placeholder="Auto Model"
+              onChange={handleChange}
+              className={errors.autoModel && touched.autoModel && "error"}
+            />
+            {errors.autoModel && touched.autoModel && (
+              <div className="input-error">{errors.autoModel}</div>
+            )}
 
-        <input
-          type="number"
-          min="300"
-          max="850"
-          id="creditScore"
-          name="creditScore"
-          value={creditScore}
-          placeholder="Credit Score"
-          onChange={(e) => setCreditScore(e.target.value)}
-        />
+            <label htmlFor="yearlyIncome"> Yearly Income: </label>
 
-        <button type="submit" alt="submit">
-          {" "}
-          Submit
-        </button>
-      </form>
-    </div>
+            <input
+              type="integer"
+              id="yearlyIncome"
+              name="yearlyIncome"
+              value={values.yearlyIncome}
+              placeholder="Yearly Income"
+              onChange={handleChange}
+              className={errors.yearlyIncome && touched.yearlyIncome && "error"}
+            />
+            {errors.yearlyIncome && touched.yearlyIncome && (
+              <div className="input-error">{errors.yearlyIncome}</div>
+            )}
+            <label htmlFor="creditScore"> Credit Score: </label>
+
+            <input
+              type="number"
+              min="300"
+              max="850"
+              id="creditScore"
+              name="creditScore"
+              value={values.creditScore}
+              placeholder="Credit Score"
+              onChange={handleChange}
+              className={errors.creditScore && touched.creditScore && "error"}
+            />
+            {errors.creditScore && touched.creditScore && (
+              <div className="input-error">{errors.creditScore}</div>
+            )}
+            <> </>
+
+            <button type="submit" alt="submit">
+              {" "}
+              Submit
+            </button>
+          </form>
+        );
+      }}
+    </Formik>
   );
 };
 
--- src/components/NewAccout.js
+++ src/components/NewAccout.js
@@ -1,52 +1,102 @@
 import React, { Fragment, useState } from "react";
+import { Formik } from "formik";
+import * as EmailValidator from "email-validator";
+import * as Yup from "yup";
 
 // - New Account Page
 //   If the api call does not return a disqualification message(see below), this page
 //   should have a simple account creation form including:
 
-//   Username (Text)
-//   Password (Text)
+//  [x] Username (Text)
+//   [x] Password (Text)
 
-// The username should be validated as an email and password should require more than 8
+// [x]The username should be validated as an email and password should require more than 8
 // characters and a number or special character. Ensure the user types their password
 // twice to validate their intent.
 
-const NewAccout = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setUsername("");
-    setPassword("");
-  };
-  return (
-    <Fragment>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username"> User Name : </label>
-        <input
-          type="text"
-          id="username"
-          alt="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password"> Password: </label>
-
-        <input
-          type="text"
-          id="password"
-          alt="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="Submit" alt="submit">
-          Submit
-        </button>
-      </form>
-    </Fragment>
-  );
-};
-
+const NewAccout = () => (
+  <Formik
+    initialValues={{ email: "", password: "", passwordconfrimation: "" }}
+    onSubmit={(values, { setSubmitting, resetForm }) => {
+      setTimeout(() => {
+        console.log("Logging in...", values);
+        setSubmitting(false);
+        resetForm();
+      }, 500);
+    }}
+    validationSchema={Yup.object().shape({
+      email: Yup.string().email().required("No Email Provided"),
+      password: Yup.string()
+        .required("No Password Provided.")
+        .min(9, "Password is too short - should have 9 chars minimum.")
+        .matches(
+          /(?=.*[@$!%*#?&, 0-9])/,
+          "Password must contain a number or a special character."
+        ),
+      passwordconfrimation: Yup.string()
+        .required("No Password Provided.")
+        .test("passwords-match", "Passwords must match", function (value) {
+          return this.parent.password === value;
+        }),
+    })}
+  >
+    {(props) => {
+      const {
+        values,
+        touched,
+        errors,
+        isSubmitting,
+        handleChange,
+        handleSubmit,
+      } = props;
+      return (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            value={values.email}
+            onChange={handleChange}
+            className={errors.email && touched.email && "error"}
+          />
+          {errors.email && touched.email && (
+            <div className="input-feedback">{errors.email}</div>
+          )}
+          <label htmlFor="email">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            className={errors.password && touched.password && "error"}
+          />
+          {errors.password && touched.password && (
+            <div className="input-feedback">{errors.password}</div>
+          )}
+          <label htmlFor="email">Password</label>
+          <input
+            name="passwordconfrimation"
+            type="password"
+            placeholder="Re-Enter your password"
+            value={values.passwordconfrimation}
+            onChange={handleChange}
+            className={
+              errors.passwordconfrimation &&
+              touched.passwordconfrimation &&
+              "error"
+            }
+          />
+          {errors.passwordconfrimation && touched.passwordconfrimation && (
+            <div className="input-feedback">{errors.passwordconfrimation}</div>
+          )}
+          <button type="submit" disabled={isSubmitting}>
+            Login
+          </button>
+        </form>
+      );
+    }}
+  </Formik>
+);
 export default NewAccout;
